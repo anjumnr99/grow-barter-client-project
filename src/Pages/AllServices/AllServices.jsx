dd9cdd280147c0bf408064e45bf585e47cdac548
anjumnr99 <anjumanowara99@gmail.com>
2023-11-06 15:08:59 +0600

    all services is completed with more button

--- src/Pages/AllServices/AllServices.jsx
+++ src/Pages/AllServices/AllServices.jsx
@@ -4,8 +4,9 @@ import AllServicesCard from "./AllServicesCard";
 
 
 const AllServices = () => {
-    
+
     const [services, setServices] = useState([]);
+    const [isMore, setIsMore] = useState(false);
 
     useEffect(() => {
         axios.get('http://localhost:5000/services')
@@ -15,13 +16,36 @@ const AllServices = () => {
             })
     }, []);
 
-    console.log(services);
-    
+    const handleMore = () => {
+        setIsMore(true);
+    }
+
+    console.log(services.length);
+
     return (
         <div>
-             {
-                services?.map(service => <AllServicesCard key={service._id} service={service} ></AllServicesCard>)
-             }
+            {
+
+                isMore ? <div>
+                    {
+                        services?.map(service => <AllServicesCard key={service._id} service={service} ></AllServicesCard>)
+                    }
+                </div> : <div>
+                    {
+                        services?.slice(0, 6).map(service => <AllServicesCard key={service._id} service={service} ></AllServicesCard>)
+                    }
+
+                </div>
+
+            }
+
+            {
+                services.length > 6 && <div className="flex justify-center items-center my-5">
+                    <button onClick={handleMore} type="button" className={`py-3 px-4 ${isMore&&'hidden'} inline-flex justify-center items-center gap-2 rounded-md bg-white/[.1] border border-transparent font-bold text-green-500 hover:text-green-600 hover:bg-green-100 focus:outline-none focus:ring-2 ring-offset-white focus:ring-white focus:ring-offset-2 transition-all text-lg dark:focus:ring-offset-gray-800`}>
+                        More...
+                    </button>
+                </div>
+            }
         </div>
     );
 };
--- src/Pages/AllServices/AllServicesCard.jsx
+++ src/Pages/AllServices/AllServicesCard.jsx
@@ -6,7 +6,7 @@ const AllServicesCard = ({service}) => {
 
     return (
         <div>
-            <div  className="flex flex-col  md:h-96 p-3 my-4 mx-2 md:mx-3 lg:mx-auto items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-5xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div  className="flex flex-col  md:h-96 p-3 my-4 mx-2 md:mx-3 lg:mx-auto items-center bg-green-100 border border-green-400 rounded-lg shadow md:flex-row md:max-w-5xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <img className="object-cover w-full rounded-t-lg h-96 md:h-full md:w-[50%] md:rounded-none md:rounded-l-lg" src={Service_Image} alt="" />
                 <div className="flex flex-col justify-between p-4 leading-normal">
                     <div className="flex-shrink-0 group block">
@@ -29,37 +29,6 @@ const AllServicesCard = ({service}) => {
                     </div>
                 </div>
             </div>
-
-
-
-
-
-            {/* <div className="flex flex-col h-96 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row w-[80%] mx-auto hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img className="object-cover w-full h-[50%] lg:h-full rounded-t-lg  md:h-auto md:w-48  md:rounded-none md:rounded-l-lg lg:w-[50%]" src="https://i.ibb.co/F8PrZ6m/Artisan-Bread-Exchange.jpg" alt="" />
-                <div className="flex flex-col border h-[50%] lg:w-[50%] lg:h-full  p-4 leading-normal">
-                    <div className="flex-shrink-0 mb-5 group block">
-                        <div className="flex items-center">
-                            <img className="inline-block flex-shrink-0 h-[3.875rem] w-[3.875rem] rounded-full" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80" alt="Image Description" />
-                            <div className="ml-3">
-                                <h3 className="font-semibold text-gray-800 dark:text-white">Maria Wanner</h3>
-                                <p className="text-sm font-medium text-gray-400">4 hr ago</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                    <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Service name</h5>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Service description</p>
-                    <p>Service Area</p>
-                    <div className="flex mt-4 justify-between  space-x-2 text-xl dark:text-gray-400">
-                        <p>Price : $</p>
-                        <button type="button" className="flex font-semibold items-center p-1 space-x-1.5">
-                            View Details
-                        </button>
-                    </div>
-                    </div>
-
-                </div>
-            </div> */}
 
         </div>
     );
